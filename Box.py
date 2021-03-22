1be3e9e295201ba3aaa8c9782cb6a5e5c74791fd
bcaceres <brittanyjcaceres@gmail.com>
2021-03-22 16:23:26 +0000

    Made infinite scroll smoother

--- Box.py
+++ Box.py
@@ -1,5 +1,8 @@
 import random
 from Queue import Queue
+from time import sleep
+from getkey import getkey, keys
+from threading import Thread
 
 class Box:
   rPlaces = 3
@@ -8,6 +11,12 @@ class Box:
   bLen = 10+3
   box=Queue()
   money = int(50)
+  IMG_life = "❤︎"
+  receptionists = ["👩‍💼", "👨‍💼"]
+  lives = 3
+  running = True
+  level, visitors, seen=0, 0, 0
+  speed = [0.20, 0.20, 0.15, 0.10, 0.08, 0.05]
 
   def __init__(self):
     self.createBox()
@@ -19,7 +28,9 @@ class Box:
 
   def printBox(self):
     #make a print thing
-    print('Budget: '+"{}".format(self.money))
+    print("\033[H",end="")
+    print((self.IMG_life+" ")*self.lives)
+    print('Budget: '+"{}".format(self.money), "")
     print('+'+('-'*self.numR*3)+'+')
     print('|'+' R '*self.numR+'|')
     self.box.display()
@@ -27,14 +38,23 @@ class Box:
     
   def roundG(self):
     addRec = random.randint(0, 4)
-    if addRec != 0:
+    if addRec != 0 or self.visitors==10:
       self.box.enqueue('|'+(' '*self.numR*3) +'|')
-      self.box.dequeue()
-      self.money+=1
     else:
       bInd = int((self.numR+3)/2)-1
       self.box.enqueue('|'+(' '*bInd)+ 'V' +(' '*bInd)+'|')
-      self.box.dequeue()
+      self.visitors+=1
+    if self.box.dequeue().find('V')>-1:
+      self.seen+=1
       self.money+=10
-    
+    else:
+      self.money+=1
     self.printBox()
+
+  def run(self):
+    while self.running:
+      self.roundG()
+      sleep(self.speed[0]) #sleep(speed[level])
+      if self.seen ==10:
+        self.running=False
+      
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from cursor import hide, show
 
 hide()
 world = Box()
-for i in range(25):
-  world.roundG()
+world.run()
 
 show()
